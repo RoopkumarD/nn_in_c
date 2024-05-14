8ca9e6a73233052f57133cfa82a2faf42c18c0a1
RoopkumarD <dasroopkumar5@gmail.com>
2024-05-15 01:39:44 +0530

    final improvement according to architecture

--- shallow_nn.c
+++ shallow_nn.c
@@ -1,11 +1,14 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "mat.h"
 #include "utils.h"
 
 double cost_function(Matrix *activation, Matrix *train_y, int train_x_length);
 void feed_forward(
 	Matrix *train_x,
+	int training_length,
 	Matrix *weights[],
 	Matrix *bias[],
 	int num_layer
@@ -22,7 +25,7 @@ int X[4][2] = {
 	{1,1},
 };
 
-int Y[4] = {0, 1, 1, 0};
+int Y[4] = {0, 1, 0, 1};
 
 int layers[] = {2, 2, 1};
 int num_layers = sizeof(layers) / sizeof(int);
@@ -32,8 +35,11 @@ int epoch = 1000;
 float learn_rate = 5;
 
 int main(void) {
+	srand(time(NULL));
+
 	int retval = 0;
 	Matrix *X_mat = NULL;
+	Matrix *train_x_separate[training_length];
 	Matrix *Y_mat = NULL;
 	// because can't do dynamical sized array at file level
 	Matrix *weights[num_layers-1];
@@ -54,10 +60,7 @@ int main(void) {
 
 	inside the scope as after below line, we are into its' scope
         */
-        Matrix *zs[compute_layers];
         Matrix *activations[num_layers];
-        Matrix *delta[compute_layers];
-        Matrix *delta_weights[compute_layers];
 
 	// initialising random weights, bias, activations and zs
 	for (int i = 0; i < compute_layers; i++) {
@@ -85,40 +88,56 @@ int main(void) {
 			retval = 1;
 			goto cleanup;
 		}
-		matrix_zero_init(acti);
 		activations[i+1] = acti;
+	}
 
-		Matrix *del = Matrix_create(layers[i+1], training_length);
-		if (del == NULL) {
-			puts("Failed to create delta matrix");
-			retval = 1;
-			goto cleanup;
+	// zs for temp storage while matrix multiplication
+	int high_value_of_nodes = 0;
+	for (int m = 1; m < num_layers; m++) {
+		if (high_value_of_nodes < layers[m]) {
+			high_value_of_nodes = layers[m];
 		}
-		matrix_zero_init(del);
-		delta[i] = del;
-
-		Matrix *del_w = Matrix_create(layers[i+1], layers[i]);
-		if (del_w == NULL) {
-			puts("Failed to create delta weights matrix");
-			retval = 1;
-			goto cleanup;
-		}
-		matrix_zero_init(del_w);
-		delta_weights[i] = del_w;
-
-		Matrix *zsm = Matrix_create(layers[i+1], training_length);
-		if (zsm == NULL) {
-			puts("Failed to create z matrix");
-			retval = 1;
-			goto cleanup;
-		}
-		matrix_zero_init(zsm);
-		zs[i] = zsm;
+	}
+	Matrix *zs = Matrix_create(high_value_of_nodes, training_length);
+	if (zs == NULL) {
+		puts("Failed to create zs matrix");
+		retval = 1;
+		goto cleanup;
+	}
+	Matrix *delta = Matrix_create(high_value_of_nodes, training_length);
+	if (delta == NULL) {
+		puts("Failed to create delta matrix");
+		retval = 1;
+		goto cleanup;
+	}
+	Matrix *delta_mul = Matrix_create(high_value_of_nodes, training_length);
+	if (delta_mul == NULL) {
+		puts("Failed to create delta_mul matrix");
+		retval = 1;
+		goto cleanup;
+	}
+	Matrix *delta_weights = Matrix_create(high_value_of_nodes, high_value_of_nodes);
+	if (delta_weights == NULL) {
+		puts("Failed to create delta_weights matrix");
+		retval = 1;
+		goto cleanup;
 	}
 
 	// converting X and y into matrix form
 	int X_rows = NUM_ROWS(X);
 	int X_cols = NUM_COLS(X);
+	for (int i = 0; i < training_length; i++) {
+		train_x_separate[i] = Matrix_create(layers[0], 1);
+		if (train_x_separate[i] == NULL) {
+			puts("Failed to create matrix for train_x_separate");
+			retval = 1;
+			goto cleanup;
+		}
+		for (int j = 0; j < layers[0]; j++) {
+			train_x_separate[i]->data[j] = X[i][j];
+		}
+	}
+
 	X_mat = Matrix_create(X_rows, X_cols);
 	if (X_mat == NULL) {
 		puts("Failed to create matrix for X");
@@ -147,23 +166,14 @@ int main(void) {
 	Y_mat->transpose = 1;
 
 	// first matrix of activations
-	/*
-	activations[0] = Matrix_create(X_mat->rows, X_mat->cols);
-	if (activations[0] == NULL) {
-		puts("Failed to create matrix for activations[0]");
-		retval = 1;
-		goto cleanup;
-	}
-	matrix_copy(X_mat, activations[0]);
-	*/
 	activations[0] = X_mat;
-	matrix_print(activations[0]);
 
 	// Now training the model
 	for (int i = 0; i < epoch; i++) {
 		for (int j = 0; j < compute_layers; j++) {
 			// z = np.dot(w, z) + b
-			int able_to_multiply = matrix_mul(weights[j], activations[j], zs[j]);
+			zs->rows = weights[j]->rows;
+			int able_to_multiply = matrix_mul(weights[j], activations[j], zs);
 			if (able_to_multiply == 1) {
 				puts("mat1:");
 				matrix_print(weights[j]);
@@ -172,9 +182,9 @@ int main(void) {
 				retval = 1;
 				goto cleanup;
 			}
-			matrix_add(zs[j], bias[j], zs[j]);
+			matrix_add(zs, bias[j], zs);
 			// z = sigmoid(z)
-			sigmoid(zs[j], activations[j+1]);
+			sigmoid(zs, activations[j+1]);
 		}
 		/*
 		printing the current result
@@ -187,35 +197,48 @@ int main(void) {
 
 		// delta = (activations[-1] - training_y) * sigmoid_prime(zs[-1])
 		matrix_scalar_mul(Y_mat, -1);
-		matrix_add(activations[num_layers-1], Y_mat, delta[compute_layers-1]);
+		delta->rows = activations[num_layers-1]->rows;
+		matrix_add(activations[num_layers-1], Y_mat, delta);
 		matrix_scalar_mul(Y_mat, -1);
-		sigmoid_prime(activations[num_layers-1], zs[compute_layers-1]);
-		matrix_multiply(delta[compute_layers-1], zs[compute_layers-1], delta[compute_layers-1]);
+		zs->rows = activations[num_layers-1]->rows;
+		sigmoid_prime(activations[num_layers-1], zs);
+		matrix_multiply(delta, zs, delta);
 
 		double weighted_learn_rate = -1 * learn_rate/training_length;
 		double restore_value = -1 * training_length/learn_rate;
 
 		// updating bias
 		// delta_b[-1] = np.sum(delta, axis=1).reshape((-1, 1))
-		matrix_scalar_mul(delta[compute_layers-1], weighted_learn_rate);
+		matrix_scalar_mul(delta, weighted_learn_rate);
 		// b - ((self.learn_rate / len(training_x)) * db)
-		int total_elem = delta[compute_layers-1]->cols * delta[compute_layers-1]->rows;
+		int total_elem = delta->cols * delta->rows;
 		for (int k = 0; k < total_elem; k++) {
-			bias[compute_layers-1]->data[k/delta[compute_layers-1]->cols] 
-				+= delta[compute_layers-1]->data[k];
+			bias[compute_layers-1]->data[k/delta->cols] += delta->data[k];
 		}
-		matrix_scalar_mul(delta[compute_layers-1], restore_value);
+		matrix_scalar_mul(delta, restore_value);
 
 		// updating weights
 		// delta_w[-1] = np.dot(delta, activations[-2].transpose())
 		matrix_transpose(activations[num_layers-2]);
-		matrix_mul(delta[compute_layers-1], activations[num_layers-2], delta_weights[compute_layers-1]);
+		delta_weights->rows = weights[compute_layers-1]->rows;
+		delta_weights->cols = weights[compute_layers-1]->cols;
+		int able_to_multiply = matrix_mul(delta, activations[num_layers-2], delta_weights);
+		if (able_to_multiply == 1) {
+			puts("mat1:");
+			matrix_print(delta);
+			puts("mat2:");
+			matrix_print(activations[num_layers-2]);
+			puts("res:");
+			matrix_print(delta_weights);
+			retval = 1;
+			goto cleanup;
+		}
 		matrix_transpose(activations[num_layers-2]);
 		// w - ((self.learn_rate / len(training_x)) * dw)
-		matrix_scalar_mul(delta_weights[compute_layers-1], weighted_learn_rate);
-		total_elem = delta_weights[compute_layers-1]->cols * delta_weights[compute_layers-1]->rows;
+		matrix_scalar_mul(delta_weights, weighted_learn_rate);
+		total_elem = delta_weights->cols * delta_weights->rows;
 		for (int k = 0; k < total_elem; k++) {
-			weights[compute_layers-1]->data[k] += delta_weights[compute_layers-1]->data[k];
+			weights[compute_layers-1]->data[k] += delta_weights->data[k];
 		}
 
 		for (int m = 2; m < num_layers; m++) {
@@ -226,38 +249,62 @@ int main(void) {
 		    	)
 			*/
 			matrix_transpose(weights[compute_layers-m+1]);
-			matrix_mul(weights[compute_layers-m+1], delta[compute_layers-m+1], delta[compute_layers-m]);
+			// cols because above transpose
+			delta_mul->rows = weights[compute_layers-m+1]->cols;
+			int able_to_multiply = matrix_mul(weights[compute_layers-m+1], delta, delta_mul);
+			if (able_to_multiply == 1) {
+				puts("mat1:");
+				matrix_print(weights[compute_layers-m+1]);
+				puts("mat2:");
+				matrix_print(delta);
+				puts("res:");
+				matrix_print(delta_mul);
+				retval = 1;
+				goto cleanup;
+			}
 			matrix_transpose(weights[compute_layers-m+1]);
-			sigmoid_prime(activations[num_layers-m], zs[compute_layers-m]);
-			matrix_multiply(delta[compute_layers-m], zs[compute_layers-m], delta[compute_layers-m]);
+			zs->rows = activations[num_layers-m]->rows;
+			sigmoid_prime(activations[num_layers-m], zs);
+			matrix_multiply(delta_mul, zs, delta_mul);
 			
 			// updating bias
 			// delta_b[-i] = np.sum(delta, axis=1, keepdims=True)
-			matrix_scalar_mul(delta[compute_layers-m], weighted_learn_rate);
+			matrix_scalar_mul(delta_mul, weighted_learn_rate);
 			// b - ((self.learn_rate / len(training_x)) * db)
-			int total_elem = delta[compute_layers-m]->cols * delta[compute_layers-m]->rows;
+			int total_elem = delta_mul->cols * delta_mul->rows;
 			for (int k = 0; k < total_elem; k++) {
-				bias[compute_layers-m]->data[k/delta[compute_layers-m]->cols] 
-					+= delta[compute_layers-m]->data[k];
+				bias[compute_layers-m]->data[k/delta_mul->cols] += delta_mul->data[k];
 			}
-			matrix_scalar_mul(delta[compute_layers-m], restore_value);
+			matrix_scalar_mul(delta_mul, restore_value);
 
 			// updating weights
 			// delta_w[-i] = np.dot(delta, activations[-i - 1].transpose())
 			matrix_transpose(activations[num_layers-m-1]);
-			matrix_mul(delta[compute_layers-m], activations[num_layers-m-1], delta_weights[compute_layers-m]);
+			delta_weights->rows = weights[compute_layers-m]->rows;
+			delta_weights->cols = weights[compute_layers-m]->cols;
+			matrix_mul(delta_mul, activations[num_layers-m-1], delta_weights);
 			matrix_transpose(activations[num_layers-m-1]);
 			// w - ((self.learn_rate / len(training_x)) * dw)
-			matrix_scalar_mul(delta_weights[compute_layers-m], weighted_learn_rate);
-			total_elem = delta_weights[compute_layers-m]->cols * delta_weights[compute_layers-m]->rows;
+			matrix_scalar_mul(delta_weights, weighted_learn_rate);
+			total_elem = delta_weights->cols * delta_weights->rows;
 			for (int k = 0; k < total_elem; k++) {
-				weights[compute_layers-m]->data[k] += delta_weights[compute_layers-m]->data[k];
+				weights[compute_layers-m]->data[k] += delta_weights->data[k];
 			}
+
+			// copying delta_mul to delta
+			delta->rows = delta_mul->rows;
+			matrix_copy(delta_mul, delta);
 		}
 	}
 	
 	// Then feed forward to check the result
-	feed_forward(X_mat, weights, bias, num_layers);
+	// separting each train for checking
+	for (int m = 0; m < training_length; m++) {
+		puts("For Input: ");
+		matrix_print(train_x_separate[m]);
+		feed_forward(train_x_separate[m], 1, weights, bias, num_layers);
+	}
+	// feed_forward(X_mat, weights, bias, num_layers);
 
 	// above goto will jump to this line for execution
 	// if goto is not invoked, still below will process as normally
@@ -266,14 +313,23 @@ int main(void) {
 	// freeing all the memory asked
 	matrix_free(X_mat);
 	matrix_free(Y_mat);
+	zs->rows = high_value_of_nodes;
+	matrix_free(zs);
+	delta->rows = high_value_of_nodes;
+	matrix_free(delta);
+	delta_mul->rows = high_value_of_nodes;
+	matrix_free(delta_mul);
+	delta_weights->rows = high_value_of_nodes;
+	delta_weights->cols = high_value_of_nodes;
+	matrix_free(delta_weights);
 	// matrix_free(activations[0]);
+	for (int i = 0; i < training_length; i++) {
+		matrix_free(train_x_separate[i]);
+	}
 	for (int i = 0; i < compute_layers; i++) {
 		matrix_free(weights[i]);
 		matrix_free(bias[i]);
 		matrix_free(activations[i+1]);
-		matrix_free(zs[i]);
-		matrix_free(delta[i]);
-		matrix_free(delta_weights[i]);
 	}
 
 	return retval;
@@ -298,53 +354,83 @@ double cost_function(Matrix *activation, Matrix *train_y, int train_x_length) {
 
 void feed_forward(
 	Matrix *train_x,
+	int training_length,
 	Matrix *weights[],
 	Matrix *bias[],
 	int num_layer
 ) {
-        Matrix *zs[num_layer-1];
-        Matrix *activations[num_layers];
-	for (int i = 0; i < num_layer-1; i++) {
-		Matrix *acti = Matrix_create(layers[i+1], training_length);
-		if (acti == NULL) {
-			puts("Failed to create activation matrix for feedforward");
-			goto cleanup;
-		}
-		activations[i+1] = acti;
-
-		Matrix *zsm = Matrix_create(layers[i+1], training_length);
-		if (zsm == NULL) {
-			puts("Failed to create z matrix for feedforward");
-			goto cleanup;
-		}
-		zs[i] = zsm;
+	if (train_x == NULL) {
+		puts("train_x is NULL");
+		goto cleanup;
 	}
 
-	activations[0] = train_x;
+	int high_value_of_nodes = 0;
+	for (int m = 1; m < num_layers; m++) {
+		if (high_value_of_nodes < layers[m]) {
+			high_value_of_nodes = layers[m];
+		}
+	}
+	Matrix *zs = Matrix_create(high_value_of_nodes, training_length);
+	if (zs == NULL) {
+		puts("Failed to create zs matrix for feedforward");
+		goto cleanup;
+	}
+	Matrix *activations = Matrix_create(high_value_of_nodes, training_length);
+	if (activations == NULL) {
+		puts("Failed to create zs matrix for feedforward");
+		goto cleanup;
+	}
+
+	// setting activation to train_x
+	// need to worry about transpose
+	int rows, cols;
+	int m = train_x->transpose;
+	if (m == 0) {
+		rows = train_x->rows;
+		cols = train_x->cols;
+	} else if (m == 1) {
+		rows = train_x->cols;
+		cols = train_x->rows;
+	}
+
+	if (activations->cols != cols) {
+		puts("Bro send data in (input row) * (obs cols) format");
+		goto cleanup;
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			activations->data[(1-m)*(i * cols + j) + m * (i + j * rows)] = 
+				train_x->data[(1-m)*(i * cols + j) + m * (i + j * rows)];
+		}
+	}
+	activations->rows = rows;
 
 	for (int j = 0; j < num_layer-1; j++) {
 		// val = sigmoid(np.dot(W, x) + b)
-		int able_to_multiply = matrix_mul(weights[j], activations[j], zs[j]);
+		zs->rows = weights[j]->rows;
+		int able_to_multiply = matrix_mul(weights[j], activations, zs);
 		if (able_to_multiply == 1) {
 			puts("mat1:");
 			matrix_print(weights[j]);
 			puts("mat2:");
-			matrix_print(activations[j]);
+			matrix_print(activations);
 			goto cleanup;
 		}
-		matrix_add(zs[j], bias[j], zs[j]);
+		matrix_add(zs, bias[j], zs);
 		// z = sigmoid(z)
-		sigmoid(zs[j], activations[j+1]);
+		activations->rows = zs->rows;
+		sigmoid(zs, activations);
 	}
 
 	puts("Result of Feed Forward is:");
-	matrix_print(activations[num_layer-1]);
+	matrix_print(activations);
 
 	cleanup:
-	for (int i = 0; i < num_layer-1; i++) {
-		matrix_free(activations[i+1]);
-		matrix_free(zs[i]);
-	}
+	activations->rows = high_value_of_nodes;
+	zs->rows = high_value_of_nodes;
+	matrix_free(activations);
+	matrix_free(zs);
 
 	return;
 }
